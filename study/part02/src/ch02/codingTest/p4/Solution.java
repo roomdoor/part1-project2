690a75cb6576680c2250a47d50e29731a57d1628
roomdoor <roomdoor.lee@gmail.com>
2022-06-04 16:47:25 +0900

    0602 코테 수정

--- study/part02/src/ch02/codingTest/p4/Solution.java
+++ study/part02/src/ch02/codingTest/p4/Solution.java
@@ -10,8 +10,12 @@ public class Solution {
             totalTime += time;                  // 현재시간에 각 시간을 더함
 
             if (totalTime >= delay) {           // 더한 시간이 delay 시간보다 커지면 용량을 줄여야함
-                totalTime -= delay;             // 현재 용량을 하나 줄이면서 현재 시간에서 delay 만큼 빼줌
-                curCap--;                       // 이 작업을 먼저 하는 이유는 문제에 제거하는 작업을 먼저 한다고 적혀있어서!!
+                int mul = totalTime / delay;
+                totalTime -= mul * delay;
+                curCap -= mul;
+                if (curCap < 0) {               // 현재 용량을 하나 줄이면서 현재 시간에서 delay 만큼 빼줌
+                    curCap = 0;                   // 이 작업을 먼저 하는 이유는 문제에 제거하는 작업을 먼저 한다고 적혀있어서!!
+                }
             }
 
             if (curCap >= capacity) {           // 만약 현재 용량이 최대 용량보다 같거나 크다면
@@ -25,6 +29,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(solution(3, 1, new int[]{0,0,0,0,3}));
+        System.out.println(solution(3, 1, new int[]{0, 0, 0, 0, 3}));
     }
 }
