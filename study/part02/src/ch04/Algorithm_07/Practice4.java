d73e752b48db4bf77660849cfc2ffe9bd85d9ee8
roomdoor <roomdoor.lee@gmail.com>
2022-06-28 13:29:53 +0900

    6/28

--- study/part02/src/ch04/Algorithm_07/Practice4.java
+++ study/part02/src/ch04/Algorithm_07/Practice4.java
@@ -54,7 +54,7 @@ public class Practice4 {
     }
 
     public static void main(String[] args) {
-        // Test code
+        // Test code:wq
         int[] nums = {-1, 0, 1, 2, -1, -4};     // [[-1, -1, 2], [-1, 0, 1]]
         System.out.println(solution(nums));
 
