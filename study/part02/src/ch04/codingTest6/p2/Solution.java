89e9f21d6b024091d5ba6e203ff78c0b16c68b3a
roomdoor <roomdoor.lee@gmail.com>
2022-07-02 16:05:44 +0900

    7/2

--- study/part02/src/ch04/codingTest6/p2/Solution.java
+++ study/part02/src/ch04/codingTest6/p2/Solution.java
@@ -8,20 +8,19 @@ public class Solution {
         int peopleNum = stomach.length;
         int totalNum = 0;
 
-        int[][] set = new int[amount.length][2];
+        int[][] meet = new int[amount.length][2];
         for (int i = 0; i < amount.length; i++) {
-            set[i][0] = amount[i];
-            set[i][1] = value[i];
+            meet[i][0] = amount[i];
+            meet[i][1] = value[i];
         }
 
-        Arrays.sort(set, (x, y) -> -x[1] + y[1]);
+        Arrays.sort(meet, (x, y) -> -x[1] + y[1]);
 
-        if (set[0][0] >= peopleNum) {
-
-            answer += (set[0][0] / peopleNum) * set[0][1] * peopleNum;
+        if (meet[0][0] >= peopleNum) {
+            answer += (meet[0][0] / peopleNum) * meet[0][1] * peopleNum;
         }
         for (int i = 0; i < peopleNum; i++) {
-            stomach[i] -= (set[0][0] / peopleNum);
+            stomach[i] -= (meet[0][0] / peopleNum);
             totalNum += stomach[i];
         }
 
@@ -30,11 +29,12 @@ public class Solution {
             if (totalNum == 0) {
                 break;
             }
-            if (totalNum >= set[i][0]) {
-                answer += set[i][0] * set[i][1];
-                totalNum -= set[i][0];
+            if (totalNum >= meet[i][0]) {
+                answer += meet[i][0] * meet[i][1];
+                totalNum -= meet[i][0];
             } else {
-                answer += totalNum * set[i][1];
+                answer += totalNum * meet[i][1];
+                totalNum = 0;
             }
         }
 
@@ -43,9 +43,21 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] a = new int[]{10,1,1,1};
-        int[] b = new int[]{10,1,1,1};
+        int[] a = new int[]{10, 1, 1, 1};
+        int[] b = new int[]{10, 1, 1, 1};
         int[] c = new int[]{1, 2, 2, 2};
+//        System.out.println(solution(a, b, c));
+
+        a = new int[]{11, 17};
+        b = new int[]{4, 8};
+        c = new int[]{5, 19, 16, 5, 4, 4, 2, 15, 20, 19, 20, 20, 3, 9, 13, 5, 2, 4, 10, 17, 2, 4, 10, 10, 8, 6, 13, 18, 13, 5, 11, 2, 8, 10, 13, 8, 16, 8, 7, 14, 5, 20, 6, 16, 15, 16, 7, 9, 1, 7, 11, 2, 8, 10, 17, 9, 2, 13, 5, 9, 19, 20, 12, 15, 20, 9, 2, 15, 10, 3, 6, 1, 12, 16, 2, 13, 2, 12, 2, 8, 1, 13, 5, 16, 13, 4, 6, 14, 15, 9, 12, 7, 20, 10, 13, 5, 6, 17, 16, 10};
+
+//        System.out.println(solution(a, b, c));
+
+        a = new int[]{7, 6, 3, 15, 18, 12, 7, 16, 11};
+        b = new int[]{20, 18, 2, 12, 8, 7, 17, 19, 15};
+        c = new int[]{8, 13, 14, 7, 3, 14};
+
         System.out.println(solution(a, b, c));
     }
 }
--- study/part02/src/ch04/codingTest6/p3/Solution.java
+++ study/part02/src/ch04/codingTest6/p3/Solution.java
@@ -1,7 +1,12 @@
 package ch04.codingTest6.p3;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static int solution(String[] ingredients, String[] items) {
@@ -9,79 +14,124 @@ public class Solution {
             return items.length;
         }
 
-        int[] count = new int[ingredients.length];
-        boolean[] check = new boolean[items.length];
+        int answer = items.length;
 
-        HashMap<String, Integer> set = new HashMap<>();
-        for (int i = 0; i < ingredients.length; i++) {
-            set.put(ingredients[i], i);
-        }
 
-        int start = items.length - 1;
-        int end = 0;
+        Set<String> set = Arrays.stream(ingredients).collect(Collectors.toSet());
 
-        for (int i = 0; i < items.length; i++) {
-            if (set.containsKey(items[i])) {
-                count[set.get(items[i])]++;
-                check[i] = true;
+        HashMap<String, Integer> map = new HashMap<>();
 
-                start = Math.min(start, i);
-                end = Math.max(end, i);
-            }
-        }
+        int start = 0;
+        int end = 1;
+        map.put(items[start], map.getOrDefault(items[start], 0) + 1);
+        map.put(items[end], map.getOrDefault(items[end], 0) + 1);
 
-        boolean s = true;
-        boolean e = true;
-        while (s || e) {
-            if (count[set.get(items[start])] - 1 > 0) {
-                count[set.get(items[start])] -= 1;
-                while (true) {
-                    if (check[++start]) {
-                        break;
-                    }
+        while (start < end) {
+
+
+            if (map.keySet().containsAll(set)) {
+                answer = Math.min(answer, end - start);
+                if (map.get(items[start]) == 1) {
+                    map.remove(items[start]);
+                } else {
+                    map.put(items[start], map.get(items[start]) - 1);
                 }
+                start++;
             } else {
-                s = false;
+                if (end < items.length - 1) {
+                    end++;
+                } else {
+                    break;
+                }
+                map.put(items[end], map.getOrDefault(items[end], 0) + 1);
             }
 
-            if (count[set.get(items[end])] - 1 > 2) {
-                count[set.get(items[end])] -= 1;
-                while (true) {
-                    if (check[--end]) {
-                        break;
-                    }
-                }
-            } else {
-                e = false;
-            }
 
         }
-        return end - start + 1;
 
+        return answer + 1;
+    }
+
+    public static int answerReader(int problemNum, int accOrEff, int tcNum) throws IOException {
+        int answer = 0;
+        String acOrEc = accOrEff == 0 ? "/acc_test" : "/eff_test";
+        String fileName = "/Users/isihwa/workspace/zerobase/강의자료/코테_답안/0630/테스트케이스/problem"
+                + problemNum
+                + acOrEc
+                + "/" + tcNum + "_o.txt";
+
+
+        FileReader fileReader = new FileReader(fileName);
+        int a;
+        while ((a = fileReader.read()) != -1) {
+            answer *= 10;
+            answer += a - '0';
+        }
+
+        return answer;
 
     }
 
-    public static void main(String[] args) {
+    public static String[][] testCaseReader(int problemNum, int accOrEff, int tcNum) throws IOException {
+        String[][] result = new String[2][];
+
+        String[] a = new String[]{};
+        String[] b = new String[]{};
+
+
+        String acOrEc = accOrEff == 0 ? "/acc_test" : "/eff_test";
+        String fileName = "/Users/isihwa/workspace/zerobase/강의자료/코테_답안/0630/테스트케이스/problem"
+                + problemNum
+                + acOrEc
+                + "/" + tcNum + "_i.txt";
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String str;
+        StringBuilder sb = new StringBuilder();
+        while ((str = br.readLine()) != null) {
+            sb.append(str);
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        boolean in = false;
+        int k = 0;
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '[') {
+                sb1 = new StringBuilder();
+                in = true;
+            } else if (sb.charAt(i) == ']') {
+                in = false;
+                result[k++] = sb1.toString().split(" ");
+            } else if (sb.charAt(i) == ',') {
+
+            } else if (in) {
+                sb1.append(sb.charAt(i));
+            }
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
         String[] a = new String[]{"우유", "시리얼", "우유", "우유"};
         String[] b = new String[]{"우유", "물", "위스키", "막걸리", "소주", "김치냉장고", "시리얼"};
 
         System.out.println(solution(a, b));
 
-        a = new String[]{"생닭", "인삼", "소주", "대추"};
-        b = new String[]{"물", "커피", "생닭", "소주", "사탕", "생닭", "대추", "쌀", "물", "인삼", "커피", "생닭", "소주", "사탕", "생닭", "대추", "쌀", "물", "인삼", "커피", "생닭", "소주", "사탕", "생닭", "대추", "쌀"};
 
-        System.out.println(solution(a, b));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 1; j <= 5; j++) {
+                String[][] tc = testCaseReader(3, i, j);
 
-        a = new String[]{"a", "b", "c"};
-        b = new String[]{"a", "k", "k", "k", "k", "k", "k", "k", "b", "c",
-                "a", "k", "k", "k", "b", "c", "k", "k", "k", "k",
-                "a", "k", "k", "k", "k", "k", "k", "k", "b", "c",
-                "a", "k", "k", "k", "b", "c"};
-//        System.out.println(solution(a, b));
+                System.out.println(j + " 번" + "풀이 : " + solution(tc[0], tc[1]));
+                System.out.println(j + " 번" + "답안 : " + answerReader(3, i, j) + "\n");
 
-        a = new String[]{"a", "a"};
-        b = new String[]{"a", "1", "a"};
-        System.out.println(solution(a, b));
+            }
+
+        }
+
+
     }
 
 
