be0c622c02c972adc5bdba8d26f5a2c4bb1ea472
roomdoor <93379456+roomdoor@users.noreply.github.com>
2022-08-06 19:52:42 +0900

    Delete study directory

--- study/part02/src/ch01/math09_1/PracticeN.java
+++
@@ -1,7 +0,0 @@
-package ch01.math09_1;//
-
-public class PracticeN {
-    public static void main(String[] args) {
-
-    }
-}
--- study/part05/src/ch04/code1/Student.java
+++
@@ -1,8 +0,0 @@
-package ch04.code1;
-
-public class Student {
-
-    public final int age = 20;
-    private String name;
-
-}
